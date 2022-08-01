4003055adf5b124b02ba570bfb633e5540f4aef2
raoakif <akifrao@gmail.com>
2022-08-01 20:55:47 +0500

    Add validations to user, category and transaction models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
   belongs_to :users
 
+  validates: :name, presence: true, length: { minimum: 3, maximum: 50 }
+  validates: :icon, presence: true
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,7 @@
 class Transaction < ApplicationRecord
   belongs_to :user
-  
+
+  validates: :user_id, presence: true
+  validates: :name, presence: true, length: { minimum: 3, maximum: 50 }
+  validates: :amount, presence: true
 end
