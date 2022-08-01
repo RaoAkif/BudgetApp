40e39643777c2544c33d3cade6af02ad26254daf
raoakif <akifrao@gmail.com>
2022-08-01 21:12:36 +0500

    Add associations between category and transaction

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ApplicationRecord
   belongs_to :users
+  has_many :transactions
 
   validates: :name, presence: true, length: { minimum: 3, maximum: 50 }
   validates: :icon, presence: true
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,5 +1,6 @@
 class Transaction < ApplicationRecord
   belongs_to :user
+  has_many: categories
 
   validates: :user_id, presence: true
   validates: :name, presence: true, length: { minimum: 3, maximum: 50 }
