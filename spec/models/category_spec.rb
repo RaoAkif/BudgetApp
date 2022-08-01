2441d6af3b9c1dd9694fa34d3fbd95fb8acc5481
raoakif <akifrao@gmail.com>
2022-08-01 22:25:24 +0500

    Remove Rubocop Linter errors

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Category, type: :model do
   before :each do
-    @category = Category.create(name: "Shopping", icon: "www.icon.com")
+    @category = Category.create(name: 'Shopping', icon: 'www.icon.com')
   end
 
   describe 'Category Model Properties' do
@@ -10,7 +10,7 @@ RSpec.describe Category, type: :model do
       @category.name = nil
       expect(@category).to_not be_valid
     end
-  
+
     it 'should not have a nill Icon field' do
       @category.icon = nil
       expect(@category).to_not be_valid
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
   before :each do
-    @transaction = Transaction.create(user_id: 1, name: "Shopping", amount: 200.50)
+    @transaction = Transaction.create(user_id: 1, name: 'Shopping', amount: 200.50)
   end
 
   describe 'Transaction Model Properties' do
@@ -10,12 +10,12 @@ RSpec.describe Transaction, type: :model do
       @transaction.user_id = nil
       expect(@transaction).to_not be_valid
     end
-    
+
     it 'should not have a nill Name field' do
       @transaction.name = nil
       expect(@transaction).to_not be_valid
     end
-  
+
     it 'should not have a nill amount field' do
       @transaction.amount = nil
       expect(@transaction).to_not be_valid
